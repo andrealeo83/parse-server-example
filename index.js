a9fc901bf6126f4c29ab7477d299ba2e7f2362e6
dariodepa <czone555@gmail.com>
2017-05-11 12:04:53 +0200

    cambio bundleId

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ var api = new ParseServer({
         //pfx: __dirname + '/cloud/RukkuCertificatoProduzione.p12',
         pfx: __dirname + '/cloud/RukkuCertificatoSviluppo.p12',
         passphrase: '', // optional password to your p12/PFX
-        bundleId: 'it.frontiere21.Rukku',
+        bundleId: 'it.rukku.rukku',
+        //bundleId: 'it.frontiere21.Rukku',
         //production: true
         production: false
       }
